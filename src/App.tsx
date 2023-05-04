31929188537407502ca67d080011adf3d058e5b5
Shakir <sshakir56@gmail.com>
2023-05-04 16:35:22 +0530

    moved useeffect inside app fun

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,23 @@ import { bugAdded, bugRemoved, bugResolved } from "./components/reducer";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
-const dispatch = useDispatch();
-
-useEffect(() => {
-  dispatch(bugAdded({ description: "Bug 1" }));
-}, [dispatch]);
-
 function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">{/* your components */}</div>
-    </Provider>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(bugAdded({ description: "Bug 1" }));
+    dispatch(bugAdded({ description: "Bug 2" }));
+    dispatch(bugAdded({ description: "Bug 3" }));
+  }, [dispatch]);
+
+  return <div className="App">{/* your components */}</div>;
 }
 
 // store.dispatch(bugResolved({ id: 1 }));
 // store.dispatch(bugRemoved({ id: 1 }));
 
-export default App;
+export default () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
